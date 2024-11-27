54d14912161411aecfacce475094e474cd1d9f22
Enzo DO ROSARIO <contact@edorosario.fr>
2024-11-27 20:11:58 +0100

    initial commit with first test

---
+++ src/hexagon/use-cases/add-ai/add-ai.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("Feature: Add ai", () => {
+  test("Scenario: it should add a new ai correctly", () => {
+    expect(true).toBe(true);
+  });
+});

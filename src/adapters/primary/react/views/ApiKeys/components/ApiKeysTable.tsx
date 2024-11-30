0df73583d56cd81dff34612f86081d9d5828452b
Enzo DO ROSARIO <contact@edorosario.fr>
2024-11-30 11:39:45 +0100

    some refactoring

--- src/adapters/primary/react/views/ApiKeys/components/ApiKeysTable.tsx
+++ src/adapters/primary/react/views/ApiKeys/components/ApiKeysTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { apiKeysSelector } from "../../../selectors/api-keys-selector.ts";
 import {
@@ -9,9 +10,25 @@ import {
   TableRow,
 } from "../../../components/Table.tsx";
 import { Badge } from "../../../components/Badge.tsx";
+import { ApiKey } from "../../../../../../store/appState.ts";
 
 export const ApiKeysTable = () => {
   const { apiKeys, loading } = useSelector(apiKeysSelector);
+  const [showLoading, setShowLoading] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = loading
+      ? setTimeout(() => setShowLoading(true), 200)
+      : undefined;
+    if (!loading) {
+      setShowLoading(false);
+    }
+    return () => {
+      if (timeoutId !== undefined) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [loading]);
 
   return (
     <Table>
@@ -23,7 +40,7 @@ export const ApiKeysTable = () => {
         </TableRow>
       </TableHead>
       <TableBody>
-        {loading
+        {showLoading
           ? [...Array(2)].map((_, index) => (
               <TableRow key={`skeleton-${index}`}>
                 <TableCell className="animate-pulse">
@@ -38,20 +55,24 @@ export const ApiKeysTable = () => {
               </TableRow>
             ))
           : apiKeys.map((apiKey) => (
-              <TableRow key={apiKey.id}>
-                <TableCell>{apiKey.name}</TableCell>
-                <TableCell>{apiKey.key}</TableCell>
-                <TableCell>
-                  {apiKey.type === "OPEN_AI" && (
-                    <Badge color="lime">OpenAI</Badge>
-                  )}
-                  {apiKey.type === "ANTHROPIC" && (
-                    <Badge color="amber">Anthropic</Badge>
-                  )}
-                </TableCell>
-              </TableRow>
+              <ApiKeyRow key={apiKey.id} apiKey={apiKey} />
             ))}
       </TableBody>
     </Table>
+  );
+};
+
+const ApiKeyRow = (props: { apiKey: ApiKey }) => {
+  const badgeFromType = {
+    OPEN_AI: <Badge color="lime">OpenAI</Badge>,
+    ANTHROPIC: <Badge color="amber">Anthropic</Badge>,
+  };
+
+  return (
+    <TableRow>
+      <TableCell>{props.apiKey.name}</TableCell>
+      <TableCell>{props.apiKey.key}</TableCell>
+      <TableCell>{badgeFromType[props.apiKey.type]}</TableCell>
+    </TableRow>
   );
 };
